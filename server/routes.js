e4cdc6cdc41c092a0ca8809cea4b44921ef370fd
divyamprabhudessai <144110931+divyamprabhudessai@users.noreply.github.com>
2024-03-08 14:55:21 +0530

    routes.js

--- server/routes.js
+++ server/routes.js
@@ -42,10 +42,23 @@ router.get('/players',async(req,res)=>{
     }
 })
 
+router.get('/getPlayers/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const response = await TestModel.findById(id); // Just pass id directly, no need for {_id:id}
+        console.log(response);
+        res.send(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error'); // Handle errors properly
+    }
+});
+
+
 router.post('/insert',async(req,res)=>{
     try{
         const newData =  await TestModel.create(req.body)
-        res.send('data insereted succesfully')
+        res.send(req.body)
     }
     catch(err){
         console.error(err)
